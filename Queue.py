412fb3f842fa1ca49dc1274ef8cef9f00ef6bc1f
John Hunt <john.hunt10@gmail.com>
2020-11-11 17:56:11 +0000

    Queue example

---
+++ Queue.py
@@ -0,0 +1,23 @@
+class Queue:
+    """ Queue ADT """
+
+    def __init__(self):
+        self.data = []
+
+    def is_empty(self):
+        return self.data == []
+
+    def enqueue(self, item):
+        self.data.insert(0, item)
+
+    def dequeue(self):
+        return self.data.pop()
+
+    def size(self):
+        return len(self.data)
+
+    def clear(self):
+        self.data = []
+
+    def __str__(self):
+        return 'Queue' + self.data
---
+++ queue-example.py
@@ -0,0 +1,15 @@
+from Queue import Queue
+
+queue = Queue()
+
+print('queue.is_empty():', queue.is_empty())
+
+queue.enqueue("First")
+queue.enqueue("Second")
+queue.enqueue("Third")
+
+print('queue.size():', queue.size())
+
+print('first dequeue:', queue.dequeue())
+print('second dequeue:', queue.dequeue())
+print('third dequeue:', queue.dequeue())
