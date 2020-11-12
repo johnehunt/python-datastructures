0e27c9ecc3c41bc7e0d5e605c3b80492c74c630d
John Hunt <john.hunt10@gmail.com>
2020-11-12 16:37:11 +0000

    Added linked list example

---
+++ linked_list_example.py
@@ -0,0 +1,29 @@
+from linked_list import Node
+from linked_list import LinkedList
+
+n1 = Node('First')
+print(n1)
+
+
+ll = LinkedList()
+print('ll.is_empty():', ll.is_empty())
+
+ll.add(42)
+ll.add(12)
+ll.add(21)
+ll.add(10)
+
+print('ll.is_empty():', ll.is_empty())
+print('ll.size():', ll.size())
+
+print(ll)
+
+print('ll.contains(21):', ll.contains(21))
+print('ll.find(21):', ll.find(21))
+
+ll.remove(12)
+print(ll)
+
+ll.insert(75, 21)
+print(ll)
+
