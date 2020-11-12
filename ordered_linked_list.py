16f7b5202abe5cd70b44a427e83a521278a91361
John Hunt <john.hunt10@gmail.com>
2020-11-12 16:57:27 +0000

    Example for an ordered linked list

---
+++ ordered_linked_list.py
@@ -0,0 +1,112 @@
+class Node:
+    """ Represents a node within a Linked List ADT"""
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, data):
+        self.data = data
+
+    def set_next(self, new_next):
+        self.next = new_next
+
+    def __str__(self):
+        return 'Node(' + str(self.data) + ')'
+
+
+class OrderedLinkedList:
+
+    # Following methods are the same as for an unorder linked list
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def size(self):
+        current = self.head
+
+        count = 0
+        while current is not None:
+            count = count + 1
+            current = current.get_next()
+
+        return count
+
+    def remove(self, item):
+        current = self.head
+
+        previous = None
+        found = False
+        while not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        if previous is None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+
+    def __str__(self):
+        current = self.head
+        result = '[ '
+        end_of_data = False
+        while not end_of_data:
+            if current is not None:
+                result = result + str(current) + ' '
+                current = current.get_next()
+            else:
+                end_of_data = True
+        result = result + ']'
+        return result
+
+    # Differences relative to an unordered linked list - and no insert
+
+    def add(self, item):
+        """ Add a value into the correct location within the list """
+        current = self.head
+        previous = None
+        stop = False
+        while current is not None and not stop:
+            if current.get_data() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        temp = Node(item)
+        if previous is None:
+            temp.set_next(self.head)
+            self.head = temp
+        else:
+            temp.set_next(current)
+            previous.set_next(temp)
+
+    def find(self, item):
+        """ Modified to exit when we have passed the point
+        where the node should be located. This makes it
+        more efficient. """
+        current = self.head
+        found = False
+        stop = False
+        while current is not None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            else:
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
+
+        return found
---
+++ ordered_linked_list_example.py
@@ -0,0 +1,9 @@
+from ordered_linked_list import OrderedLinkedList
+
+oll = OrderedLinkedList()
+
+oll.add(23)
+oll.add(15)
+oll.add(35)
+
+print('oll:', oll)
